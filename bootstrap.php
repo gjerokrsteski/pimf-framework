65874950eabe4147fda2e13c26dad8192d293707
Firstname Lastname <gjero@krsteski.de>
2012-10-10 09:46:38 +0200

    ensure ini files parsing for php5.2

--- bootstrap.php
+++ bootstrap.php
@@ -71,10 +71,8 @@ if (version_compare(PHP_VERSION, $config->bootstrap->expected->php_version, '<')
 
 // check expected extensions.
 foreach ($config->bootstrap->expected->extensions as $extension) {
- if (!extension_loaded($extension)) {
-   if (!dl($extension.'.so')) {
-      $problems[] = 'No '.$extension.' extension loaded!';
-    }  
+  if (!extension_loaded($extension)) {
+    $problems[] = 'No ' . $extension . ' extension loaded!';
   }
 }
 
--- core/Pimf/Util/IniParser.php
+++ core/Pimf/Util/IniParser.php
@@ -61,6 +61,36 @@ class Pimf_Util_IniParser
    */
   public function process($src)
   {
+    // workaround for PHP 5 < 5.3.0
+    if (!function_exists('parse_ini_string')) {
+      function parse_ini_string($str, $ProcessSections = false)
+      {
+        $lines  = explode("\n", $str);
+        $return = array();
+        $inSect = false;
+        foreach ($lines as $line) {
+          $line = trim($line);
+          if (!$line || $line[0] == "#" || $line[0] == ";") {
+            continue;
+          }
+          if ($line[0] == "[" && $endIdx = strpos($line, "]")) {
+            $inSect = substr($line, 1, $endIdx - 1);
+            continue;
+          }
+          if (!strpos($line, '=')) {
+            continue;
+          }
+          $tmp = explode("=", $line, 2);
+          if ($ProcessSections && $inSect) {
+            $return[$inSect][trim($tmp[0])] = ltrim($tmp[1]);
+          }else{
+            $return[trim($tmp[0])] = ltrim($tmp[1]);
+          }
+        }
+        return $return;
+      }
+    }
+
     $simpleParsed      = parse_ini_string($src, true);
     $inheritanceParsed = array();
 
