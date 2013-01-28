5a608d0d56a01f7d291e6cfdb1d04b6775699643
Firstname Lastname <gjero@krsteski.de>
2013-01-28 16:49:46 +0100

    no words :-)

--- core/Pimf/Resolver/Exception.php
+++ core/Pimf/Resolver/Exception.php
@@ -1,8 +1,26 @@
 <?php
 /**
- * @namespace   Pimf_Resolver_Exception
- * @author      Gjero Krsteski <gjero.krsteski@questback.de>
- * @copyright   (c) 1999-2012 QuestBack AG http://www.questback.de/
+ * Pimf_Resolver
+ *
+ * PHP Version 5
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://krsteski.de/new-bsd-license/
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to gjero@krsteski.de so we can send you a copy immediately.
+ *
+ * @copyright Copyright (c) 2010-2011 Gjero Krsteski (http://krsteski.de)
+ * @license http://krsteski.de/new-bsd-license New BSD License
+ */
+
+/**
+ * @package Pimf_Resolver
+ * @author Gjero Krsteski <gjero@krsteski.de>
  */
 class Pimf_Resolver_Exception extends RuntimeException
 {
