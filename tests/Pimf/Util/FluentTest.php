b2500f137dca5ddf1ef1d336838e7ba7c844cccd
gjerokrsteski <gjero@krsteski.de>
2013-09-19 11:20:55 +0200

    add test for Pimf_Util_Fluent accessing properties as array #php #framework

--- tests/Pimf/Util/FluentTest.php
+++ tests/Pimf/Util/FluentTest.php
@@ -16,4 +16,15 @@ class FluentTest extends PHPUnit_Framework_TestCase
     $this->assertSame(25, $person->age);
     $this->assertSame(null, $person->nullable);
   }
+
+  public function testUsingAsArrayMagic()
+  {
+    $person = new Pimf_Util_Fluent();
+
+    $person->name('Lammy')->age(25)->nullable();
+
+    $this->assertSame('Lammy', $person['name']);
+    $this->assertSame(25, $person['age']);
+    $this->assertSame(null, $person['nullable']);
+  }
 }
