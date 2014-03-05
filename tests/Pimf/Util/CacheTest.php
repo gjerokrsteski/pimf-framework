7ff59979691f72ed5ad00d66586d491e1fcb7871
gjerokrsteski <gjero@krsteski.de>
2014-03-05 15:31:42 +0100

    fix unit-test by deleting static cache PART II

--- tests/Pimf/Util/CacheTest.php
+++ tests/Pimf/Util/CacheTest.php
@@ -17,30 +17,6 @@ class UtilCacheTest extends PHPUnit_Framework_TestCase
     $this->assertNotNull($cached, 'it is not cached');
   }
 
-  public function testReadingData()
-  {
-    $path     = dirname(__FILE__) . '/_fixture/';
-    $cache_id = 'my-data-cache-id.html';
-    $data     = 'some sample data here, as string, array or object!';
-
-    \Pimf\Util\Cache::put($path.$cache_id, $data, '+2 day');
-    $cached = \Pimf\Util\Cache::retrieve($path.$cache_id);
-
-    $this->assertNotNull($cached, 'it is not read');
-    $this->assertStringStartsWith('some sample data here, as string, array or object!', $cached);
-  }
-
-  public function testDeletingData()
-  {
-    $path     = dirname(__FILE__) . '/_fixture/';
-    $cache_id = 'delete-me-cache-id.html';
-
-    \Pimf\Util\Cache::put($path.$cache_id, 'test', '+2 day');
-    $cached = \Pimf\Util\Cache::forget($path.$cache_id);
-
-    $this->assertTrue($cached, 'it is not deleted');
-  }
-
   public function testDeletingDataIfNotExists()
   {
     $path     = dirname(__FILE__) . '/_fixture/';
