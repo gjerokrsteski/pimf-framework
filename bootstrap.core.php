e9ced1749795928c720ac4261282973dc0382456
g.krsteski <g.krsteski@controlexpert.com>
2014-06-06 11:47:34 +0200

    avoid Case insensitive constant names that are not supported in HipHop

--- bootstrap.core.php
+++ bootstrap.core.php
@@ -4,8 +4,8 @@
 | PIMF bootstrap
 |--------------------------------------------------------------------------
 */
-if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR, true);
-if(!defined('BASE_PATH')) define('BASE_PATH', realpath(__DIR__) . DS, true);
+if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
+if(!defined('BASE_PATH')) define('BASE_PATH', realpath(__DIR__) . DS);
 
 require_once 'autoload.core.php';
 require_once 'utils.php';
