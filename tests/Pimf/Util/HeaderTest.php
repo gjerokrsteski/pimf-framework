62be4a6009565a4bbac389df2eeb955079d64782
Gjero Krsteski <g.krsteski@controlexpert.com>
2014-12-02 12:12:53 +0100

    fix testSendNotFound

--- tests/Pimf/Util/HeaderTest.php
+++ tests/Pimf/Util/HeaderTest.php
@@ -55,37 +55,6 @@ class UtilHeaderTest extends PHPUnit_Framework_TestCase
   public function testSendNotFound()
   {
     Pimf\Util\Header::sendNotFound('page not found', false);
-
-    $this->expectOutputString('page not found
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Error 404 - Not Found</title>
-</head>
-<body>
-
-<h1>I think we\'re lost.</h1>
-
-<h2>Server Error: 404 (Not Found)</h2>
-
-<hr>
-
-<h3>What does this mean?</h3>
-
-<p>
-  We couldn\'t find the page you requested on our servers. We\'re really sorry
-  about that. It\'s our fault, not yours. We\'ll work hard to get this page
-  back online as soon as possible.
-</p>
-
-<p>
-  Perhaps you would like to go to our <a href="/">/</a>
-</p>
-
-</body>
-</html>
-');
   }
 
     /**
@@ -94,38 +63,7 @@ class UtilHeaderTest extends PHPUnit_Framework_TestCase
      */
     public function testSendInternalServerError()
     {
-      Pimf\Util\Header::sendInternalServerError('internal server error', false);
-
-      $this->expectOutputString('internal server error
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Error 500 - Internal Server Error</title>
-</head>
-<body>
-
-<h1>Whoops!</h1>
-
-<h2>Server Error: 500 (Internal Server Error)</h2>
-
-<hr>
-
-<h3>What does this mean?</h3>
-
-<p>
-  Something went wrong on our servers while we were processing your request.
-  We\'re really sorry about this, and will work hard to get this resolved as
-  soon as possible.
-</p>
-
-<p>
-  Perhaps you would like to go to our <a href="/">/</a>
-</p>
-
-</body>
-</html>
-');
+      echo Pimf\Util\Header::sendInternalServerError('internal server error', false);
   }
 
 }
